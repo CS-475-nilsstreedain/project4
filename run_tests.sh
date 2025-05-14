350231a0d0ee324fc82f8458ee414f479116c029
Nils Streedain <25465133+nilsstreedain@users.noreply.github.com>
2025-05-14 16:40:34 -0700

    Fix script output

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@
 
 # Create output file
 OUTPUT_FILE="results.txt"
-echo "Array Size\tNon-SIMD Mul (MM/s)\tSIMD Mul (MM/s)\tSpeedup\tNon-SIMD MulSum (MM/s)\tSIMD MulSum (MM/s)\tSpeedup" > $OUTPUT_FILE
+echo -e "Array Size\tNon-SIMD Mul (MM/s)\tSIMD Mul (MM/s)\tSpeedup\tNon-SIMD MulSum (MM/s)\tSIMD MulSum (MM/s)\tSpeedup" > $OUTPUT_FILE
 
 # Array sizes to test (from 1K to 8M)
 SIZES=(1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608)
@@ -11,7 +11,8 @@ for SIZE in "${SIZES[@]}"
 do
     echo "Testing array size: $SIZE"
     g++ -fopenmp all04.cpp -o all04 -DARRAYSIZE=$SIZE
-    ./all04 >> $OUTPUT_FILE
+    RESULT=$(./all04 2>&1)
+    echo -e "$RESULT" | sed 's/^[ \t]*//;s/[ \t]*$//' >> $OUTPUT_FILE
 done
 
 echo "Testing complete. Results saved to $OUTPUT_FILE"
